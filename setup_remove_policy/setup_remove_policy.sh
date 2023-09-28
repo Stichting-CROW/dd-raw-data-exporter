efa08c5a2d481806d48fe20d0ffdae481e4e3219
Sven Boor <sven.boor@gmail.com>
2023-09-28 07:19:41 +0200

    Improved remove policy.

--- setup_remove_policy/setup_remove_policy.sh
+++ setup_remove_policy/setup_remove_policy.sh
@@ -5,4 +5,6 @@ aws s3api put-bucket-lifecycle-configuration \
     --endpoint https://ams3.digitaloceanspaces.com \
     --lifecycle-configuration file://remove_policy.json
 
-aws s3api get-bucket-lifecycle-configuration --bucket dashboarddeelmobiliteit
+aws s3api get-bucket-lifecycle-configuration \
+    --bucket dashboarddeelmobiliteit \
+    --endpoint https://ams3.digitaloceanspaces.com
